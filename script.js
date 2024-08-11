768bb943dc0eae5a4e4e13ed8a21678747f6b013
thxlenSoju <maytheforcebewu@proton.me>
2024-08-11 10:20:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+	autoplay: {
+	  delay: 5000,
+	},
+	loop: true,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+  });
